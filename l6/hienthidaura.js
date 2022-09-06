87686d9f3898bbc60b01f072318158dc488f70de
dangquoctrung2410 <104485466+dangquoctrung2410@users.noreply.github.com>
2022-09-06 15:06:11 +0700

    uplesson6

---
+++ l6/hienthidaura.js
@@ -0,0 +1,12 @@
+function showmessage() {
+    alert("xinchao!")
+}
+function showMessage1(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+function showMessage2(){
+    document.write('Xin chào!');
+}
+function showMessage3(){
+    console.log('Xin chào!');
+}
